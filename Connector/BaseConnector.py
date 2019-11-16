87ff4dbd0b17b7b949dfb3c9de05764b049107d3
Hen Shalom <xdrakolax@gmail.com>
2019-11-16 21:55:25 +0200

    add base function to BaseConnector

--- Connector/BaseConnector.py
+++ Connector/BaseConnector.py
@@ -1,3 +1,9 @@
+from Queries import AdvanceQuery
+
+
 class BaseConnector:
     def __init__(self, schema):
         self.schema = schema
+
+    def query_data(self, advance_query: AdvanceQuery):
+        raise NotImplementedError()
