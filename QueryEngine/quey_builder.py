b52939df3ed1927336136c5a15ca1d8f1a5d214c
Hen Shalom <xdrakolax@gmail.com>
2019-11-16 23:34:48 +0200

    update functions return value

--- QueryEngine/quey_builder.py
+++ QueryEngine/quey_builder.py
@@ -8,9 +8,11 @@ from QueryEngine.Parser.query_parser import generate_basic_query
 def generate_basic_queries(query: dict, group: TaggingGroup):
     basic_query = generate_basic_query(query, group)
     settings = group.settings
-    basic_query.return_list = group.keys_dict.keys()
+    basic_query.return_list = list(group.keys_dict.values())
     basic_query.table_name = settings.get("table", ""),
     basic_query.collection_name = settings.get("collection", "")
+    basic_query.schema_name = settings.get("schema", "")
+    return basic_query
 
 
 def create_advance_queries(basic_queries: List[BasicQuery]):
@@ -20,4 +22,4 @@ def create_advance_queries(basic_queries: List[BasicQuery]):
         if not advance_queries.get(schema_name):
             advance_queries[schema_name] = AdvanceQuery([], schema_name=schema_name)
         advance_queries[schema_name].add_query(basic_query)
-    return advance_queries
+    return advance_queries.values()
