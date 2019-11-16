c448e6f5e18dbd0c19769c89ee54ed4764ff9ca0
Hen Shalom <xdrakolax@gmail.com>
2019-11-16 22:23:31 +0200

    add basic query bulder

---
+++ QueryEngine/quey_builder.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from Queries import AdvanceQuery, BasicQuery
+from QueryEngine import TaggingGroup
+from QueryEngine.Parser.query_parser import generate_basic_query
+
+
+def generate_basic_queries(query: dict, group: TaggingGroup):
+    basic_query = generate_basic_query(query, group)
+    settings = group.settings
+    basic_query.return_list = group.keys_dict.keys()
+    basic_query.table_name = settings.get("table", ""),
+    basic_query.collection_name = settings.get("collection", "")
+
+
+def create_advance_queries(basic_queries: List[BasicQuery]):
+    advance_queries = dict()
+    for basic_query in basic_queries:
+        schema_name = basic_query.schema_name
+        if not advance_queries.get(schema_name):
+            advance_queries[schema_name] = AdvanceQuery([], schema_name=schema_name)
+        advance_queries[schema_name].add_query(basic_query)
+    return advance_queries
