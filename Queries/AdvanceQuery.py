b2880e8a149ad2bcd54ad4e442280b12e4177e42
Hen Shalom <xdrakolax@gmail.com>
2019-11-16 21:54:29 +0200

    change connector to schema_name

--- Queries/AdvanceQuery.py
+++ Queries/AdvanceQuery.py
@@ -1,7 +1,7 @@
 class AdvanceQuery:
-    def __init__(self, basic_queries=list, connector=None):
+    def __init__(self, basic_queries=list, schema_name=None):
         self.basic_queries = basic_queries if isinstance(basic_queries, list) else [basic_queries]
-        self.connector = connector
+        self.schema_name = schema_name
 
     def add_query(self, query):
         self.basic_queries.append(query)
