ecbacdd70fbfb8c01046682daebaa5f00180b1db
Hen Shalom <xdrakolax@gmail.com>
2019-11-16 21:55:11 +0200

    add schema_name to BasicQuery

--- Queries/BasicQuery.py
+++ Queries/BasicQuery.py
@@ -3,13 +3,13 @@ from const import RETURN_ALL, AND_OPERATOR
 
 class BasicQuery:
     def __init__(self, query_items, table_name=None, collection_name=None,
-                 operator=AND_OPERATOR,
-                 return_list=RETURN_ALL):
+                 operator=AND_OPERATOR, return_list=RETURN_ALL, schema_name=None):
         self.operator = operator
         self.query_items = query_items if isinstance(query_items, list) else [query_items]
         self.table_name = table_name
         self.collection_name = collection_name
         self.return_list = return_list
+        self.schema_name = schema_name
 
     def __add__(self, basic_query):
         query_items = basic_query.query_items + self.query_items
